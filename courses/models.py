0e780c80ba3607a98c60075686fe780777940eb0
iMac <imac@iMacs-iMac.local>
2024-04-02 14:16:48 +0200

    начало проекта E-Learning

---
+++ courses/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Subject(models.Model):
+    """Класс для предметов курса"""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    """Класс для курсов"""
+    owner = models.ForeignKey(User,
+                              related_name='courses_created',
+                              on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,
+                                related_name='courses',
+                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    """Класс для модулей"""
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    #order = OrderField(blank=True, for_fields=['course'])
+    #
+    # class Meta:
+    #     ordering = ['order']
+
+    # def __str__(self):
+    #     return f"{self.order}. {self.title}"
