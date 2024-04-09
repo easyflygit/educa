53519378bd852b16688bc60fbe631f48d2f545ee
iMac <imac@iMacs-iMac.local>
2024-04-09 13:09:40 +0200

    добавил регистрацию студентов и возможность записи на курс

--- educa/settings.py
+++ educa/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # мои приложения
+    'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
