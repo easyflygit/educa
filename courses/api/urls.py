56021462fce6a028c2c3971a0154600f77eaf7de
easyflygit <akv888@inbox.ru>
2024-04-16 19:25:23 +0200

    API:добавил сериализацию содержимого курса

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 app_name = 'courses'
 
@@ -10,4 +14,14 @@ urlpatterns = [
     path('subjects/<pk>/',
          views.SubjectDetailView.as_view(),
          name='subject_detail'),
+    path('courses/',
+         views.CourseListView.as_view(),
+         name='courses_list'),
+    path('courses/<pk>/',
+         views.CourseDetailView.as_view(),
+         name='course_detail'),
+    # path('courses/<pk>/enroll/',
+    #      views.CourseEnrollView.as_view(),
+    #      name='course_enroll'),
+    path('', include(router.urls)),
 ]
